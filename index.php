5cf6ffba7fd06fa7bc9a8243fe698a65d5bbd5d0
Oliver Benoit <draper87@gmail.com>
2020-08-26 15:22:33 +0200

    second commit

--- index.php
+++ index.php
@@ -11,7 +11,48 @@
     public $data_iscrizione;
     public $commenti;
 
-    
+    public function __construct($_nome, $_cognome, $_email, $_data_iscrizione, $_commenti) {
+      $this->nome = $_nome;
+      $this->cognome = $_cognome;
+      $this->email = $_email;
+      $this->data_iscrizione = $_data_iscrizione;
+      $this->commenti = $_commenti;
+    }
+
+    public function stampaUtenti() {
+      return $this->nome . ' ' . $this->cognome . '<br>' . $this->email . '<br>' . $this->data_iscrizione . '<br>' . $this->commenti . '<br>' . '<br>';
+    }
+
+  }
+
+  // creo array di utenti
+  $utenti = [
+    [
+      'nome' => 'Oliver',
+      'cognome' => 'Benoit',
+      'email' => 'draper87@gmail.com',
+      'data_iscrizione' => '01-01-2018',
+      'commenti' => '28'
+    ],
+    [
+      'nome' => 'Francesco',
+      'cognome' => 'Rossi',
+      'email' => 'francesco@gmail.com',
+      'data_iscrizione' => '20-01-2019',
+      'commenti' => '16'
+    ],
+    [
+      'nome' => 'Filippo',
+      'cognome' => 'Testi',
+      'email' => 'filippo@libero.it',
+      'data_iscrizione' => '31-10-2019',
+      'commenti' => '7'
+    ]
+  ];
+
+  foreach ($utenti as $utente) {
+    $singoloUtente = new User($utente['nome'], $utente['cognome'], $utente['email'], $utente['data_iscrizione'], $utente['commenti']);
+    echo $singoloUtente->stampaUtenti();
   }
 
 
