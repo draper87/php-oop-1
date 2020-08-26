f7195188f74968e54cb7ad2884d8484f983ef72a
Oliver Benoit <draper87@gmail.com>
2020-08-26 15:01:43 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+  // Crea un diagramma per una tabella di db che rappresenti gli Users di un blog.
+  // Crea una classe User che rappresenti quella tabella, e usala per stampare in pagina i dati di alcuni Users.
+
+
+  // creo classe User
+  class User {
+    public $nome;
+    public $cognome;
+    public $email;
+    public $data_iscrizione;
+    public $commenti;
+
+    
+  }
+
+
+ ?>
